25045d7a55b9a194d45916e5a81f57bcfdd72125
nmaureira7-boop <nmaureira7@gmail.com>
2025-11-21 12:16:25 -0300

    Refactor DB connection to use environment variables

--- db_config.py
+++ db_config.py
@@ -11,18 +11,15 @@ DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 DB_SERVICE = os.getenv("DB_SERVICE")
 
-import oracledb
-import os
-
 import os
 import oracledb
 
 def get_connection():
     return oracledb.connect(
         user=os.getenv("DB_USER"),          # ADMIN
-        password=os.getenv("DB_PASS"),      # tu contraseña
-        dsn="g41d7b285d304e7_bluedate_high",  # servicio del wallet
+        password=os.getenv("DB_PASS"),      # contraseña del usuario
+        dsn="g41d7b285d304e7_bluedate_high",  # alias exacto del tnsnames.ora
         config_dir="/opt/render/project/src/agua_app/wallet",   # carpeta con los archivos del wallet
-        wallet_location="/opt/render/project/src/wallet",
-        wallet_password="WALLET_PASS"           # el que definiste al descargar el wallet
+        wallet_location="/opt/render/project/src/agua_app/wallet",
+        wallet_password=os.getenv("WALLET_PASS")   # clave del wallet desde variable de entorno
     )
