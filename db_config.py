6128576f5beb78291e5906591d1a76edcc2b80c8
nmaureira7-boop <nmaureira7@gmail.com>
2025-11-21 12:48:37 -0300

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -10,7 +10,7 @@ def get_connection():
         user=os.getenv("DB_USER"),          # ADMIN
         password=os.getenv("DB_PASS"),      # contraseña del usuario
         dsn="bluedate_tp",                  # alias exacto definido en tnsnames.ora
-        config_dir="/opt/render/project/src/agua_app/Wallet",   # carpeta con los archivos del wallet
-        wallet_location="/opt/render/project/src/agua_app/Wallet",
+        config_dir="/opt/render/project/src/agua_app/wallet",   # carpeta con los archivos del wallet
+        wallet_location="/opt/render/project/src/agua_app/wallet",
         wallet_password=os.getenv("WALLET_PASS")   # clave del wallet desde variable de entorno
     )
