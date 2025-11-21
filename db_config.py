bdc5163e5629815810a192902bb57b02dc07ae25
nmaureira7-boop <nmaureira7@gmail.com>
2025-11-21 12:33:34 -0300

    Refactor db_config.py for improved readability
    Updated comments for clarity and removed unused imports.

--- db_config.py
+++ db_config.py
@@ -2,23 +2,14 @@ from dotenv import load_dotenv
 import os
 import oracledb
 
-# Cargar variables desde el archivo .env
+# Cargar variables desde el archivo .env (solo en local, Render usa Environment Variables)
 load_dotenv()
-
-DB_USER = os.getenv("DB_USER")
-DB_PASS = os.getenv("DB_PASS")
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_SERVICE = os.getenv("DB_SERVICE")
-
-import os
-import oracledb
 
 def get_connection():
     return oracledb.connect(
         user=os.getenv("DB_USER"),          # ADMIN
         password=os.getenv("DB_PASS"),      # contraseña del usuario
-        dsn="bluedate_tp",                  # 👈 alias exacto del tnsnames.ora
+        dsn="bluedate_tp",                  # alias exacto definido en tnsnames.ora
         config_dir="/opt/render/project/src/agua_app/wallet",   # carpeta con los archivos del wallet
         wallet_location="/opt/render/project/src/agua_app/wallet",
         wallet_password=os.getenv("WALLET_PASS")   # clave del wallet desde variable de entorno
