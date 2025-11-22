d96eee30249317663d9543c4b524dc0f43932e15
nmaureira7-boop <nmaureira7@gmail.com>
2025-11-22 14:40:24 -0300

    Set wallet path and initialize Oracle client
    Initialize Oracle client with wallet path.

--- db_config.py
+++ db_config.py
@@ -4,16 +4,17 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+wallet_path = "/opt/render/project/src/wallet"
+
+# Inicializar el cliente Oracle SOLO una vez
+oracledb.init_oracle_client(config_dir=wallet_path)
+
 def get_connection():
-    wallet_path = "/opt/render/project/src/wallet"
     try:
         return oracledb.connect(
             user=os.getenv("DB_USER"),
             password=os.getenv("DB_PASS"),
-            dsn="bluedate_tp",
-            config_dir=wallet_path,
-            wallet_location=wallet_path,
-            wallet_password=os.getenv("WALLET_PASS")
+            dsn="bluedate_tp"  # Debe coincidir EXACTO con tu tnsnames.ora
         )
     except Exception as e:
         print("Error al conectar a Oracle:", e)
