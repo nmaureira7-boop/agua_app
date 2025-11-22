e769d145fcc33546479992ca0b0343544c3c6b36
nmaureira7-boop <nmaureira7@gmail.com>
2025-11-22 15:19:18 -0300

    Refactor Oracle client initialization in db_config.py

--- db_config.py
+++ db_config.py
@@ -4,23 +4,21 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# Importante: activar modo THIN
+oracledb.init_oracle_client = lambda *args, **kwargs: None
+
 def get_connection():
     wallet_path = "/opt/render/project/src/wallet"
-
+    
     try:
-        oracledb.init_oracle_client(
-            lib_dir="/opt/oracle/instantclient_21_3",
-            config_dir=wallet_path
-        )
-
         return oracledb.connect(
             user=os.getenv("DB_USER"),
             password=os.getenv("DB_PASS"),
-            dsn=os.getenv("DB_TNS_NAME"),  # por ej: "db202501_high"
+            dsn=os.getenv("DB_TNS_NAME"),
+            config_dir=wallet_path,
             wallet_location=wallet_path,
             wallet_password=os.getenv("WALLET_PASS")
         )
-
     except Exception as e:
-        print("Error al conectar a Oracle Autonomous:", e)
+        print("Error al conectar a Oracle:", e)
         raise
