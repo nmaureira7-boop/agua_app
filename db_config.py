9851b46e62f125e94c84d1b9677dec88e01645ba
nmaureira7-boop <nmaureira7@gmail.com>
2025-11-22 14:49:24 -0300

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -4,16 +4,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-wallet_path = "/opt/render/project/src/wallet"
-
-oracledb.init_oracle_client(config_dir=wallet_path)
-
 def get_connection():
+    wallet_path = "/opt/render/project/src/wallet"
     try:
         return oracledb.connect(
             user=os.getenv("DB_USER"),
             password=os.getenv("DB_PASS"),
-            dsn="bluedate_tp"
+            dsn="bluedate_tp",
+            config_dir=wallet_path,
+            wallet_location=wallet_path,
+            wallet_password=os.getenv("WALLET_PASS")
         )
     except Exception as e:
         print("Error al conectar a Oracle:", e)
