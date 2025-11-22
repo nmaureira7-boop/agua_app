0d67a6fb1ef0486505fa6f35ceb0b7736cc0f608
nmaureira7-boop <nmaureira7@gmail.com>
2025-11-22 14:58:34 -0300

    Update Oracle connection with environment variables

--- db_config.py
+++ db_config.py
@@ -6,15 +6,21 @@ load_dotenv()
 
 def get_connection():
     wallet_path = "/opt/render/project/src/wallet"
+
     try:
+        oracledb.init_oracle_client(
+            lib_dir="/opt/oracle/instantclient_21_3",
+            config_dir=wallet_path
+        )
+
         return oracledb.connect(
             user=os.getenv("DB_USER"),
             password=os.getenv("DB_PASS"),
-            dsn="bluedate_tp",
-            config_dir=wallet_path,
+            dsn=os.getenv("DB_TNS_NAME"),  # por ej: "db202501_high"
             wallet_location=wallet_path,
             wallet_password=os.getenv("WALLET_PASS")
         )
+
     except Exception as e:
-        print("Error al conectar a Oracle:", e)
+        print("Error al conectar a Oracle Autonomous:", e)
         raise
