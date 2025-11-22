8c1b425b1d8abce442126a0806c3e7422d373122
nmaureira7-boop <nmaureira7@gmail.com>
2025-11-22 10:39:21 -0300

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -10,10 +10,10 @@ def get_connection():
     print("Archivos en wallet:", os.listdir(wallet_path))
 
     return oracledb.connect(
-        user=os.getenv("DB_USER"),          # ADMIN
-        password=os.getenv("DB_PASS"),      # contraseña del usuario
-        dsn="bluedate_tp",                  # 👈 alias exacto del tnsnames.ora
-        config_dir=wallet_path,             # carpeta con tnsnames.ora y sqlnet.ora
-        wallet_location=wallet_path,
-        wallet_password=os.getenv("WALLET_PASS")   # clave del wallet desde variable de entorno
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASS"),
+        dsn="bluedate_tp",   # alias exacto del tnsnames.ora
+        config_dir=wallet_path,
+        wallet_location=wallet_path
+        # 👈 sin wallet_password porque tu wallet es auto-abierto
     )
