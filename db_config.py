8fcea00bed12ecfc4289606ee8f8909948f6f5e6
nmaureira7-boop <nmaureira7@gmail.com>
2025-11-22 11:09:43 -0300

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -6,14 +6,15 @@ load_dotenv()
 
 def get_connection():
     wallet_path = "/opt/render/project/src/wallet"
-
-    print("Archivos en wallet:", os.listdir(wallet_path))
-
-    return oracledb.connect(
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASS"),
-        dsn="bluedate_tp",   # alias exacto del tnsnames.ora
-        config_dir=wallet_path,
-        wallet_location=wallet_path,
-        wallet_password=os.getenv("WALLET_PASS")   # 👈 clave definida al descargar el wallet
-    )
+    try:
+        return oracledb.connect(
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASS"),
+            dsn="bluedate_tp",
+            config_dir=wallet_path,
+            wallet_location=wallet_path,
+            wallet_password=os.getenv("WALLET_PASS")
+        )
+    except Exception as e:
+        print("Error al conectar a Oracle:", e)
+        raise
