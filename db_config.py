550bf142ba708dd89103e4a35927e2e275197673
nmaureira7-boop <nmaureira7@gmail.com>
2025-11-22 15:27:41 -0300

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -7,15 +7,24 @@ load_dotenv()
 def get_connection():
     wallet_path = "/opt/render/project/src/wallet"
 
+    dsn = """
+    (description=
+        (retry_count=20)
+        (retry_delay=3)
+        (address=(protocol=tcps)(port=1522)(host=adb.sa-santiago-1.oraclecloud.com))
+        (connect_data=(service_name=g41d7b285d304e7_bluedate_tp.adb.oraclecloud.com))
+        (security=(ssl_server_dn_match=yes))
+    )
+    """
+
     try:
         return oracledb.connect(
             user=os.getenv("DB_USER"),
             password=os.getenv("DB_PASS"),
-            dsn="(description= (retry_count=20)(retry_delay=3)(address=(protocol=tcps)(port=1522)(host=adb.sa-santiago-1.oraclecloud.com))(connect_data=(service_name=bluedate_tp.adb.oraclecloud.com))(security=(ssl_server_dn_match=yes)))",
+            dsn=dsn,
             config_dir=wallet_path,
             wallet_location=wallet_path,
             wallet_password=os.getenv("WALLET_PASS"),
-            # FORZAMOS MODO THIN
             thin=True
         )
     except Exception as e:
