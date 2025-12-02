701fd6240dda89a1bfc55e5f8f4c8ecde75e08a0
nmaureira7-boop <nmaureira7@gmail.com>
2025-12-02 12:56:33 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
-# Levantar Redis en segundo plano
-redis-server --daemonize yes
-
-# Arrancar Gunicorn con Flask
-gunicorn -w 2 -t 120 -b 0.0.0.0:5000 app:app &
-
-# Arrancar Celery worker
-celery -A app.celery worker --loglevel=info
-
-chmod +x start.sh
+# Arrancar Gunicorn con Flask (tantos workers como CPUs disponibles)
+gunicorn -w $(nproc) -t 120 -b 0.0.0.0:5000 app:app &
