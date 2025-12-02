f71f6ea46b9479bdca0815c9ca6883c159b549db
nmaureira7-boop <nmaureira7@gmail.com>
2025-12-02 13:06:34 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,1 @@
-#!/bin/bash
-
-# Arrancar Gunicorn con Flask (tantos workers como CPUs disponibles)
-gunicorn -w $(nproc) -t 120 -b 0.0.0.0:5000 app:app &
+CMD ["gunicorn", "-w", "2", "-t", "120", "-b", "0.0.0.0:$PORT", "app:app"]
